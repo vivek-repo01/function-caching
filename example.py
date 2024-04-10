45e51b6504dd5a82b4d0fae8cbd502190eb96449
vivek-repo01 <vivekdikshit@yahoo.com>
2024-04-11 00:22:08 +0530

    Adding new file

---
+++ example.py
@@ -0,0 +1,22 @@
+import functools
+import time
+
+@functools.lru_cache(maxsize=None)
+def fx(n):
+    time.sleep(5)
+    return n * 5
+
+print(fx(20))
+print("Done for 20")
+print(fx(2))
+print("Done for 20")
+print(fx(6))
+print("Done for 20")
+print(fx(20))
+print("Done for 20")
+print(fx(2))
+print("Done for 20")
+print(fx(6))
+print("Done for 20")
+
+
